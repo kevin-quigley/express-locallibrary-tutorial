148f0f9ad88fb116cbbcaab9eafeef999f0ef17a
Paul  Quigley <paulquigley@Pauls-MacBook-Pro-2.local>
2021-03-29 18:19:45 -0400

    First version of application moved into github

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/cool', function(req, res, next) {
+  res.send('Youre so cool');
+});
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+
+module.exports = router;
